7cfa102a6b76901e96e98fb19cd7bcb0abbf4d0e
gilmar <gilmarjose2014@gmail.com>
2021-01-03 17:21:54 -0300

    primeiro commit

---
+++ loop_aninhado.py
@@ -0,0 +1,12 @@
+#loop aninhado
+for i in range(0,5):
+    for a in range(0,5):
+        print(a)
+
+listab = [32,53,85,10,15,17,19]
+soma = 0                        #vaiavel global
+for i in listab:
+    double_i = 1 * 2
+    soma += double_i
+
+print(soma)
